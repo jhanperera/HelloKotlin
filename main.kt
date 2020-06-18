45e3beca3208bc4eae7351939c56b36e0f36073b
Jhan Perera <jhanperera@live.com>
2020-06-17 21:05:32 -0700

    Updated LICENSE.md to point to correct name and craeted main.kt

---
+++ main.kt
@@ -0,0 +1,12 @@
+class TestClass{
+    fun print(){
+        println("TestClass function")
+    }
+}
+
+fun main() {
+    println("Hello World!")
+    var obj = TestClass()
+    obj.print()
+}
+    
